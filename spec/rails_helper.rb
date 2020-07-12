49f63a4b59b03e5d78b96f5ee31199cb42d7ea4f
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2020-07-12 13:26:57 -0400

    In order to support using policy in view templates, we need to stub pundit for our specs. Move this stub to a support/pundit file and reference it from the rails helper. Also utilize this in hunters/index spec.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,10 +12,11 @@ require 'devise'
 # Add coverage report to RSPEC
 require 'simplecov'
 SimpleCov.start
-require "pundit/rspec"
 
 # Add Factories
 require 'support/factory_bot'
+# Add auth helpers + pundit
+require 'support/pundit'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
